d84e054013717b13deb29d969ca05463c1fe9b77
Luis Dorr <Luisdorr@outlook.com>
2023-08-01 00:22:28 -0300

    add some Board and Piece atributes, constructor and get/set

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,2 +1,19 @@
-package boardgame;public class Piece {
+package boardgame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    public Piece[][] possibleMoves() {
+        return null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
 }
