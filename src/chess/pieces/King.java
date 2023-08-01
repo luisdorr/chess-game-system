33371c92af71b081af9750efd88c9e8152b0014d
Luis Dorr <Luisdorr@outlook.com>
2023-08-01 14:11:48 -0300

    create The king pieces

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,2 +1,17 @@
-package chess.pieces;public class King {
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class King extends ChessPiece {
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
 }
